1dff8cf359715a2e82f155dd8961472b2d3a670d
Colleen <colleenrust524@EMAIL.com>
2023-03-22 11:33:57 -0500

    add shirt class, add print info

--- store_item.rb
+++ store_item.rb
@@ -1,5 +1,20 @@
-shirt1 = {size: "medium", color: "red", material: "cotton", price: 20.00}
-p shirt1
+# shirt1 = {size: "medium", color: "red", material: "cotton", price: 20.00}
+# p shirt1
 
-shirt2 = {size: "large", color: "blue", material: "polyester", price: 18.00}
-p shirt2
+# shirt2 = {size: "large", color: "blue", material: "polyester", price: 18.00}
+# p shirt2
+
+class Shirt
+  def initialize(input_size, input_color, input_material, input_price)
+    @size = input_size
+    @color = input_color
+    @material = input_material
+    @price = input_price
+  end
+  def print_info
+    p "#{@size} and #{@color} shirt is #{@material} and $#{@price}. "
+  end
+
+end
+shirt1 = Shirt.new("medium","red","cotton",20.00)
+shirt1.print_info
