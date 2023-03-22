aa75216412b43a1ff49c3b819a8f5369000087a8
Colleen <colleenrust524@EMAIL.com>
2023-03-22 13:05:25 -0500

    add argument for input options, add attr reader, add attr writer, remove former initialize statements, add hash of info

--- store_item_bottoms.rb
+++ store_item_bottoms.rb
@@ -1,9 +1,12 @@
 class Bottoms
-  def initialize(input_size, input_color, input_material, input_price)
-    @size = input_size
-    @color = input_color
-    @material = input_material
-    @price = input_price
+  attr_reader :size , :color, :material, :price
+  attr_writer :size , :color, :material, :price
+
+  def initialize(input_options)
+    @size = input_options [:size]
+    @color = input_options [:color]
+    @material = input_options [:material]
+    @price = input_options [:price]
   end
   def print_info
     p "Your bottoms are size #{@size} and a beautiful shade of #{@color}  with luxurious #{@material} for only $#{@price}. "
@@ -23,9 +26,15 @@ class Bottoms
     @size = new_size
   end
 end
-pants1 = Bottoms.new("x-large","blue","cotton",70.00)
+pants1 = Bottoms.new(
+  :size =>"x-large",
+  :color => "blue",
+  :material =>"cotton",
+  :price => 70.00)
+
 pants1.material("denim")
 pants1.print_info
 
-pants2 = Bottoms.new("small","black", "leather",95.00)
-pants2.print_info
+# pants2 = Bottoms.new("small","black", "leather",95.00)
+# pants2.size("large")
+# pants2.print_info
