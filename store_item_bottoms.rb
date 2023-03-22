7f73a31ab972e95a024d00649b8c6d1e2f0e27ee
Colleen <colleenrust524@EMAIL.com>
2023-03-22 13:10:53 -0500

    remove the getter and setter methods

--- store_item_bottoms.rb
+++ store_item_bottoms.rb
@@ -11,20 +11,20 @@ class Bottoms
   def print_info
     p "Your bottoms are size #{@size} and a beautiful shade of #{@color}  with luxurious #{@material} for only $#{@price}. "
   end
-#getter/reader
-  def material
-    @material
-  end
-#setter/writer
-  def material(new_material)
-    @material = new_material
-  end 
-  def size
-    @size
-  end
-  def size(new_size)
-    @size = new_size
-  end
+# #getter/reader
+#   def material
+#     @material
+#   end
+# #setter/writer
+#   def material(new_material)
+#     @material = new_material
+#   end 
+#   def size
+#     @size
+#   end
+#   def size(new_size)
+#     @size = new_size
+#   end
 end
 pants1 = Bottoms.new(
   :size =>"x-large",
@@ -35,6 +35,10 @@ pants1 = Bottoms.new(
 pants1.material("denim")
 pants1.print_info
 
-# pants2 = Bottoms.new("small","black", "leather",95.00)
-# pants2.size("large")
-# pants2.print_info
+pants2 = Bottoms.new(
+  :size => "small",
+  :color =>"black",
+  :material => "leather",
+  :price => 95.00)
+pants2.size("large")
+pants2.print_info
